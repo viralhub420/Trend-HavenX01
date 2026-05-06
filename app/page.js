4a37d993791e3f537326cf5663e74349e968c82f
viralhub420 <hshamimbd355@gmail.com>
2026-05-06 12:04:55 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Welcome to Trend HavenX</h1>
+      <p>Our professional blog posts are coming soon!</p>
+      <a href="/studio">Go to Studio to Write Posts</a>
+    </main>
+  )
+  }
