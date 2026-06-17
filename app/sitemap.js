a98ed54e8630f3a75db1885b4a92de0dfdc8f21f
viralhub420 <hshamimbd355@gmail.com>
2026-06-17 18:45:12 +0600

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,23 @@
+import { createClient } from "next-sanity";
+
+const client = createClient({
+projectId: "y1nsnm6h",
+dataset: "production",
+apiVersion: "2024-01-01",
+useCdn: true,
+});
+
+export default async function sitemap() {
+const posts = await client.fetch("*[_type == "post"]{ "slug": slug.current, _updatedAt }");
+
+return [
+{
+url: "https://www.trendhavenx.online",
+lastModified: new Date(),
+},
+...posts.map((post) => ({
+url: "https://www.trendhavenx.online/${post.slug}",
+lastModified: post._updatedAt,
+})),
+];
+}
