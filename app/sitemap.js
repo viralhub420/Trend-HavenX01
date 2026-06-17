801d2ae60a111ec10a8f839fff34ec4ae47ee22b
viralhub420 <hshamimbd355@gmail.com>
2026-06-17 23:43:31 +0600

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,23 +1,28 @@
 import { createClient } from "next-sanity";
 
 const client = createClient({
-projectId: "y1nsnm6h",
-dataset: "production",
-apiVersion: "2024-01-01",
-useCdn: true,
+  projectId: "y1nsnm6h",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: true,
 });
 
 export default async function sitemap() {
-const posts = await client.fetch("*[_type == "post"]{ "slug": slug.current, _updatedAt }");
+  const posts = await client.fetch(
+    `*[_type == "post"]{
+      "slug": slug.current,
+      _updatedAt
+    }`
+  );
 
-return [
-{
-url: "https://www.trendhavenx.online",
-lastModified: new Date(),
-},
-...posts.map((post) => ({
-url: "https://www.trendhavenx.online/${post.slug}",
-lastModified: post._updatedAt,
-})),
-];
+  return [
+    {
+      url: "https://www.trendhavenx.online",
+      lastModified: new Date(),
+    },
+    ...posts.map((post) => ({
+      url: `https://www.trendhavenx.online/${post.slug}`,
+      lastModified: post._updatedAt,
+    })),
+  ];
 }
