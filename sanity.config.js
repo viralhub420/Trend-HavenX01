ccd8a4f8a59f917933d08bf10f8d5f0fb8c61235
viralhub420 <hshamimbd355@gmail.com>
2026-06-27 13:39:33 +0600

    Update sanity.config.js

--- sanity.config.js
+++ sanity.config.js
@@ -164,6 +164,12 @@ export default defineConfig({
           of: [
             { 
               type: 'block',
+                      styles: [
+          { title: 'Normal', value: 'normal' },
+          { title: 'Heading 2', value: 'h2' },
+          { title: 'Heading 3', value: 'h3' }
+        ],
+
               marks: {
                 annotations: [
                   // এক্সটারনাল লিংক (External Link)
